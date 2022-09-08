01cfccd8b56757999d1939be0e49b250906172a6
LarsVerschoor <98211748+LarsVerschoor@users.noreply.github.com>
2022-09-08 09:46:10 +0200

    Added updated version of the landing page

---
+++ landing-page.js
@@ -0,0 +1,24 @@
+const projects = [
+  {
+    name: 'snake',
+    url: 'https://larsverschoor.pages.dev/Snake/'
+  },
+  {
+    name: 'tic tac toe',
+    url: 'https://larsverschoor.pages.dev/Tic%20Tac%20Toe/'
+  }
+]
+
+
+
+for (let i = 0; i < projects.length; i++) {
+  const buttonElement = document.createElement('button')
+  buttonElement.innerText = projects[i].name
+  if (projects[i].url) {
+    buttonElement.addEventListener('click', () => {
+      window.open(projects[i].url)
+    })
+  }
+  buttonElement.classList = 'project-button'
+  document.body.appendChild(buttonElement)
+}
