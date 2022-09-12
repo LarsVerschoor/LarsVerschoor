02287fd389c35fb15216e0f9f9adf96c7857266a
LarsVerschoor <98211748+LarsVerschoor@users.noreply.github.com>
2022-09-12 15:51:50 +0200

    Update main.js

--- Snake/JavaScript/main.js
+++ Snake/JavaScript/main.js
@@ -3,7 +3,7 @@ import {updateFood, renderFood, initFood, removeFood} from './food.js'
 import {displayHomeScreen, settings} from './homeScreen.js'
 import {updateStatistics} from './statistics.js'
 
-if (window.confirm(I am currently designing snake v3.\nWant to see the current design?)) {
+if (window.confirm('Want to see the design of snake v3.0?')) {
   window.open("https://www.figma.com/proto/8p3brM3XVNQ6OEje79hmyL/Untitled?node-id=1%3A2&scaling=contain&page-id=0%3A1")
 }
 
