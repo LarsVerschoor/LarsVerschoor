834e9482c6a6f43fbad1f0af5bf38bdd53fe8ff1
LarsVerschoor <98211748+LarsVerschoor@users.noreply.github.com>
2022-10-04 22:48:06 +0200

    Update script.js

--- De Anaalgangers/script.js
+++ De Anaalgangers/script.js
@@ -9,8 +9,8 @@ async function getData() {
 
   const response = await fetch(endpoint, {
     headers: {
-      'method': "GET"
-      'Access-Control-Allow-Origin': 'https://larsverschoor.pages.dev'
+      'method': "GET",
+      'Access-Control-Allow-Origin': 'https://larsverschoor.pages.dev',
       'Authorization': "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjMzZThiNGJjLTgyMmMtNDY3NS04ZDgwLTAzMWM1MGNiMzBjYiIsImlhdCI6MTY2NDkxNTIyOCwic3ViIjoiZGV2ZWxvcGVyL2E1NDRhYzMwLTJhYTctNTlmMS03NjYwLWRlNGUzN2Y3NTgzYyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwMy4yMS4yNDQuMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.20Dse3oWSohx41zpSwcGE_fwSNApPC6rTzqg8UXh6lLaI5F3_va8E_BnlTYfPgkRc4_Xy_SWyYv4fsdng5q4WQ"
     }
                                
