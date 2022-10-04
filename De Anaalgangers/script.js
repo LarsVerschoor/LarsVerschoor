f113cebbb7385f07133d6b4c1e964b69edd4fa31
LarsVerschoor <98211748+LarsVerschoor@users.noreply.github.com>
2022-10-04 23:22:01 +0200

    Update script.js

--- De Anaalgangers/script.js
+++ De Anaalgangers/script.js
@@ -1,7 +1,7 @@
 async function getData() {
   const clanId = '%232YVVVOQOG'
   const endpoint = new URL(`https://api.clashofclans.com/v1/clans/${clanId}/currentwar/leaguegroup`)
-  const token = 'here i have put my token'
+  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjMxOGVjOTFjLWQ1NmItNGZkMy04MTY4LThhNTg0M2I5NjIzMiIsImlhdCI6MTY2NDkxNjk2OSwic3ViIjoiZGV2ZWxvcGVyL2E1NDRhYzMwLTJhYTctNTlmMS03NjYwLWRlNGUzN2Y3NTgzYyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE5OS4yNy4xMjguMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.bTIl6sJ5pZ6hOUWMd_qTZfvHdtKqbKuvqxFdo_1p3rESVrS0GwwPsYO5-sLgnzPhljCqkbJAIrl4UsC_lCwI9w'
 
   const response = await fetch(endpoint, {
     headers: {
