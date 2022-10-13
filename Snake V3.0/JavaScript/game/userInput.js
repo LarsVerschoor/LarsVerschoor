3c2e11cb7a26b44af545da92fa96986a6aa8be64
LarsVerschoor <98211748+LarsVerschoor@users.noreply.github.com>
2022-10-13 18:01:50 +0200

    Add files via upload

---
+++ Snake V3.0/JavaScript/game/userInput.js
@@ -0,0 +1,40 @@
+export let requestedDirection = undefined
+
+export function init() {
+  requestedDirection = undefined
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'w' || e.key === 'ArrowUp') inputDirection = 'up'
+  if (e.key === 'a' || e.key === 'ArrowLeft') inputDirection = 'left'
+  if (e.key === 's' || e.key === 'ArrowDown') inputDirection = 'down'
+  if (e.key === 'd' || e.key === 'ArrowRight') inputDirection = 'right'
+})
+
+let touchstartX
+let touchstartY
+    
+function checkDirection() {
+  if (touchendX < touchstartX) alert('swiped left!')
+  if (touchendX > touchstartX) alert('swiped right!')
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+  touchstartY = e.changedTouches[0].screenY
+})
+
+document.addEventListener('touchend', e => {
+  checkDirection(e.changedTouches[0].screenX, e.changedTouches[0].screenY)
+})
+
+function checkDirection(xEnd, yEnd) {
+  let up = touchstartX - xEnd
+  let right = touchstartY - yEnd
+  let left = -(touchstartY - yEnd)
+  let down = -(touchstartX - xEnd)
+  if (up > right && up > left && up > down) requestedDirection = 'up'
+  if (left > right && left > up && left > down) requestedDirection = 'left'
+  if (down > up && down > right && down > left) requestedDirection = 'down'
+  if (right > down && right > left && right > up) requestedDirection = 'right'
+}
