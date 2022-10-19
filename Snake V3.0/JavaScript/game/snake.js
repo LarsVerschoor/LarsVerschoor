42bab7a420b8863f75d16abd55794c9b99a7172c
Lars Verschoor <98211748+LarsVerschoor@users.noreply.github.com>
2022-10-19 14:37:39 +0200

    Update snake.js

--- Snake V3.0/JavaScript/game/snake.js
+++ Snake V3.0/JavaScript/game/snake.js
@@ -17,11 +17,7 @@ export function init() {
 }
 
 export function updateSnake() {
-  if (isOnObstacle(snakePositions[0].x, snakePositions[0].y)) {
-    snakeDeath = true
-    return
-  } 
-  if (inputDirection !== undefined) {
+   if (inputDirection !== undefined) {
     if (inputDirection === 'up' && snakeDirection !== 'down') snakeDirection = 'up'
     if (inputDirection === 'left' && snakeDirection !== 'right') snakeDirection = 'left'
     if (inputDirection === 'down' && snakeDirection !== 'up' && snakeDirection) snakeDirection = 'down'
